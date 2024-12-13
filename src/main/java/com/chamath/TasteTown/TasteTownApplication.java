bd196c83292e29734198f13dcd5c574f5b0d1bd0
Chamath Heshan <chamathheshan076@gmail.com>
2024-12-13 18:20:32 +0530

    initial commit

---
+++ src/main/java/com/chamath/TasteTown/TasteTownApplication.java
@@ -0,0 +1,13 @@
+package com.chamath.TasteTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chamath/TasteTown/TasteTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamath.TasteTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
