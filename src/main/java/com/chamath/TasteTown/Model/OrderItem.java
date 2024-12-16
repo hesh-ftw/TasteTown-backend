4b309776426d591e2dc3d904b183c975fd2d55c9
Chamath Heshan <chamathheshan076@gmail.com>
2024-12-16 17:39:10 +0530

    add OrderItem entity

---
+++ src/main/java/com/chamath/TasteTown/Model/OrderItem.java
@@ -0,0 +1,23 @@
+package com.chamath.TasteTown.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Food food;
+
+    private int quantity;
+
+    private Float totalPrice;
+}
