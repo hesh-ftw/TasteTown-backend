84b6f0834c8f49b703c55f7c4a42bb880de31545
Chamath Heshan <chamathheshan076@gmail.com>
2024-12-18 22:26:22 +0530

    add contactInformation model

---
+++ src/main/java/com/chamath/TasteTown/Model/ContactInformation.java
@@ -0,0 +1,10 @@
+package com.chamath.TasteTown.Model;
+
+import lombok.Data;
+
+@Data
+public class ContactInformation {
+    private String instagram;
+    private String facebook;
+    private String whatsapp;
+}
