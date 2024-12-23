bcb4d5150be09bdde241347e3d3539c53ddd1605
Chamath Heshan <chamathheshan076@gmail.com>
2024-12-23 16:34:36 +0530

    add userRepository

---
+++ src/main/java/com/chamath/TasteTown/Repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.chamath.TasteTown.Repositories;
+
+import com.chamath.TasteTown.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    public User findByEmail(String username);
+}
