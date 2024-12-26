d9069c92280afc3feb534f3c950002cd7d16c8d5
Chamath Heshan <chamathheshan076@gmail.com>
2024-12-26 12:55:37 +0530

    add cart repository interface

---
+++ src/main/java/com/chamath/TasteTown/Repositories/CartRepository.java
@@ -0,0 +1,9 @@
+package com.chamath.TasteTown.Repositories;
+
+import com.chamath.TasteTown.Model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart,Long> {
+}
