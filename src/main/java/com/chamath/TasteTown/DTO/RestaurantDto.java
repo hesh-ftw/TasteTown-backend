ad392e3b743c92014c9d1814b45d6b28ce335c00
Chamath Heshan <chamathheshan076@gmail.com>
2024-12-26 18:15:23 +0530

    update RestaurantDto with getters setters

--- src/main/java/com/chamath/TasteTown/DTO/RestaurantDto.java
+++ src/main/java/com/chamath/TasteTown/DTO/RestaurantDto.java
@@ -15,4 +15,36 @@ public class RestaurantDto {
 
     @Column(length = 1000)
     private List<String>image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getImage() {
+        return image;
+    }
+
+    public void setImage(List<String> image) {
+        this.image = image;
+    }
 }
