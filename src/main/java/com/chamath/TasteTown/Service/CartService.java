7466b4d183d7407c5d487df25a48482f4e8714be
Chamath Heshan <chamathheshan076@gmail.com>
2024-12-31 19:50:41 +0530

    add CartService interface

---
+++ src/main/java/com/chamath/TasteTown/Service/CartService.java
@@ -0,0 +1,22 @@
+package com.chamath.TasteTown.Service;
+
+import com.chamath.TasteTown.Model.Cart;
+import com.chamath.TasteTown.Model.CartItem;
+import com.chamath.TasteTown.Request.CartItemRequest;
+
+public interface CartService {
+
+    public CartItem addItemToCart(CartItemRequest req, String jwt)throws Exception;
+
+    public CartItem updateCartItemQuantity(Long cartItemId, int quantity)throws Exception;
+
+    public Cart removeItemFromCart(Long cartItemId, String jwt)throws Exception;
+
+    public float calculateCartTotals(Cart cart) throws Exception;
+
+    public Cart findCartById(Long id)throws Exception;
+
+    public Cart findCartByUserId(String jwt)throws Exception;
+
+    public Cart clearCart(String jwt)throws Exception;
+}
