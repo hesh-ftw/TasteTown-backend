f3c234f985962d2252d46a3dd0aeb5514b2b113b
Chamath Heshan <chamathheshan076@gmail.com>
2024-12-31 19:53:51 +0530

    add cart service API endpoints

---
+++ src/main/java/com/chamath/TasteTown/Controller/CartController.java
@@ -0,0 +1,62 @@
+package com.chamath.TasteTown.Controller;
+
+import com.chamath.TasteTown.Model.Cart;
+import com.chamath.TasteTown.Model.CartItem;
+import com.chamath.TasteTown.Request.UpdateCartItemRequest;
+import com.chamath.TasteTown.Request.CartItemRequest;
+import com.chamath.TasteTown.Service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/cart")
+@RestController
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @PutMapping("/add")
+    public ResponseEntity<CartItem> addItemToCart(@RequestBody CartItemRequest req,
+                                                  @RequestHeader("Authorization") String jwt) throws Exception {
+        CartItem cartItem = cartService.addItemToCart(req, jwt);
+
+        return new ResponseEntity<>(cartItem, HttpStatus.OK);
+    }
+
+    @PutMapping("/cart-item/update")
+    public ResponseEntity<CartItem> updateCartItemQuantity(@RequestBody UpdateCartItemRequest req,
+                                                           @RequestHeader("Authorization") String jwt) throws Exception {
+        CartItem cartItem = cartService.updateCartItemQuantity(req.getCartItemId(), req.getQuantity());
+
+        return new ResponseEntity<>(cartItem, HttpStatus.OK);
+    }
+
+    @DeleteMapping("cart-item/{id}/remove")
+    public ResponseEntity<Cart> removeItem(@PathVariable Long id,
+                                           @RequestHeader("Authorization") String jwt) throws Exception{
+
+        Cart cart=cartService.removeItemFromCart(id,jwt);
+
+        return new ResponseEntity<>(cart,HttpStatus.OK);
+    }
+
+    @PutMapping("/clear")
+    public ResponseEntity<Cart> clearCart(@RequestHeader("Authorization") String jwt) throws Exception {
+        Cart cart= cartService.clearCart(jwt);
+
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Cart> findUserCart(@RequestHeader("Authorization") String jwt) throws Exception {
+        Cart cart= cartService.clearCart(jwt);
+
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+
+
+
+
+
+}
