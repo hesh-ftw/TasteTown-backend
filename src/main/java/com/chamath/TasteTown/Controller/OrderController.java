ccfed076c2a219c546eeb841ce29bec9b69481ae
Chamath Heshan <chamathheshan076@gmail.com>
2025-01-01 20:42:33 +0530

    add endpoints of Order API

---
+++ src/main/java/com/chamath/TasteTown/Controller/OrderController.java
@@ -0,0 +1,65 @@
+package com.chamath.TasteTown.Controller;
+import com.chamath.TasteTown.Model.Order;
+import com.chamath.TasteTown.Model.User;
+import com.chamath.TasteTown.Request.OrderRequest;
+import com.chamath.TasteTown.Service.OrderService;
+import com.chamath.TasteTown.Service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    UserService userService;
+
+
+    //endpoints for users
+
+    @PostMapping("create")
+    public ResponseEntity<Order> createOrder(@RequestHeader("Authorization") String jwt,
+                                             @RequestBody OrderRequest request) throws Exception {
+        User user= userService.findUserByJwtToken(jwt);
+        Order order= orderService.createOrder(request,user);
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<Order>> getOrderHistory(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user= userService.findUserByJwtToken(jwt);
+        List<Order> orders= orderService.getUsersOrder(user.getId());
+        return new ResponseEntity<>(orders,HttpStatus.OK);
+    }
+
+
+    //endpoints for admins(restaurants owners)
+
+    @GetMapping("/admin/res/{id}")
+    public ResponseEntity<List<Order>> getRestaurantOrderHistory(@PathVariable Long id,
+                                                                 @RequestParam(required = false) String order_status,
+                                                                 @RequestHeader("Authorization") String jwt) throws Exception {
+        User user= userService.findUserByJwtToken(jwt);
+        List<Order> orders= orderService.getRestaurantsOrder(id,order_status);
+        return new ResponseEntity<>(orders,HttpStatus.OK);
+    }
+
+
+    @PutMapping("/admin/{orderId}/{orderStatus}")
+    public ResponseEntity<Order> updateOrderStatus(@PathVariable Long orderId,
+                                                         @PathVariable String orderStatus,
+                                                         @RequestHeader("Authorization") String jwt) throws Exception {
+        User user= userService.findUserByJwtToken(jwt);
+        Order orders= orderService.updateOrder(orderId,orderStatus);
+        return new ResponseEntity<>(orders,HttpStatus.OK);
+    }
+
+}
